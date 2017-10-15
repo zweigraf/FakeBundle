9b9fb6e488ac0597960345a7f8eeb8557f2c3148
Luis Reisewitz <reisewitz@me.com>
2017-10-15 21:24:01 +0200

    Make proper cmd tool

--- Package.swift
+++ Package.swift
@@ -4,10 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "FakeBundle",
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/kylef/Commander.git", .revision("e0cbee1bd73778c1076c675eaf660e97d09f3b32")),
+        // PathKit fork supporting SPM4
+        .package(url: "https://github.com/PoissonBallon/PathKit.git", .branch("master")),
+    ],
     targets: [
         .target(
             name: "FakeBundle",
-            dependencies: []),
+            dependencies: ["Commander", "PathKit"]),
     ]
 )
--- Sources/FakeBundle/main.swift
+++ Sources/FakeBundle/main.swift
@@ -1,54 +1,13 @@
 import Foundation
+import Commander
+import PathKit
 
-let baseUrl = URL(fileURLWithPath: "/Users/zweigraf/Development/gnosis/bivrost-swift/Sources/BivrostHelper")
-let outputUrlCode = URL(fileURLWithPath: "/Users/zweigraf/Development/gnosis/BivrostHelperExport.swift")
-
-let fm = FileManager.default
-
-func directoryAsCode(of url: URL) throws -> String {
-    let children = try fm.contentsOfDirectory(at: url, includingPropertiesForKeys: [], options: [.skipsSubdirectoryDescendants, .skipsHiddenFiles])
+let generateCommand = command(Option<String>("input", default: "./Resources", description: "Folder containing all the resources that should be exported. Folder name will be used as top level class name."), Option<String>("output", default: "./Resources.swift", description: "Path where the generated swift file should be stored.")) { input, output in
     
-    var childrenTypes = [String]()
-    let childrenClassesString = try children.map { url -> String in
-        var isDirectory: ObjCBool = false
-        guard fm.fileExists(atPath: url.path, isDirectory: &isDirectory) else {
-            return ""
-        }
-        
-        let filename = url.lastPathComponent
-        let type = makeTypeName(filename: filename)
-
-        // Add Children Type so we correctly add it to the children lazy var
-        guard !isDirectory.boolValue else {
-            let code = try directoryAsCode(of: url)
-            if !code.isEmpty {
-                // Only add to children if we could actually generate the type for the dir
-                childrenTypes.append("\(type)()")
-            }
-            return code
-        }
-        
-        // Single file
-        guard let data = fm.contents(atPath: url.path) else {
-            return ""
-        }
-        
-        let code = makeFile(name: type, filename: filename, contentsBase64: data.base64EncodedString())
-        if !code.isEmpty {
-            // Only add to children if we could actually generate the type for the file
-            childrenTypes.append("\(type)()")
-        }
-        return code
-    }.joined(separator: "\n\n")
+    let inputFolder = Path(input).absolute().url
+    let outputFile = Path(output).absolute().url
     
-    let filename = url.lastPathComponent
-    let name = makeTypeName(filename: filename)
-    let childrenCreation = childrenTypes.joined(separator: ", ")
-    return makeDirectory(name: name, filename: filename, childrenCreation: childrenCreation, childrenClasses: childrenClassesString)
+    try generateCode(inputUrl: inputFolder, outputUrl: outputFile)
 }
 
-func makeTypeName(filename: String) -> String {
-    return filename.replacingOccurrences(of: ".", with: "_").replacingOccurrences(of: " ", with: "__").replacingOccurrences(of: "-", with: "___").capitalized
-}
-
-try (try directoryAsCode(of: baseUrl)).data(using: .utf8)?.write(to: outputUrlCode)
+generateCommand.run()
