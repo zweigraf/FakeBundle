6f440f0c3b793e64591eed658de141a974038e05
Ludovico Rossi <ludovico.rossi@schibsted.com>
2018-10-18 15:05:15 +0100

    Add 'executable' product to support latest Mint version

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "FakeBundle",
+    products: [
+        .executable(name: "fakebundle", targets: ["fakebundle"])
+    ],
     dependencies: [
         .package(url: "https://github.com/kylef/Commander.git", .revision("e0cbee1bd73778c1076c675eaf660e97d09f3b32")),
         // PathKit fork supporting SPM4
